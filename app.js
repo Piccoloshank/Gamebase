2c0c9874b8bf39584ecf685f042dd20ca57c17ed
piccolo-shank <shashankharsh0@gmail.com>
2021-03-08 16:25:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ document.getElementById('nav-toggle').addEventListener('click', function () {
 
 
 });
+var firebaseConfig = {
+    apiKey: "AIzaSyDmGBr3mpz5NPgJIY8ocfAyZNjLJ2eGZ4c",
+    authDomain: "web-development-da6b2.firebaseapp.com",
+    projectId: "web-development-da6b2",
+    storageBucket: "web-development-da6b2.appspot.com",
+    messagingSenderId: "97161082396",
+    appId: "1:97161082396:web:11d06ac8c1b21dc28c11d9",
+    measurementId: "G-SH4MN5Q8SY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+ 
