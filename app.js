e2559a233d9a9b8c1a650a9b9c0eff0b6ffe68d9
piccolo-shank <shashankharsh0@gmail.com>
2021-03-08 15:02:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 document.getElementById('nav-toggle').addEventListener('click', function () {
     let navMenu = document.getElementById('nav-menu-container');
     navMenu.style.display = navMenu.offsetParent === null ? 'block' : 'none';
+
+
 });
